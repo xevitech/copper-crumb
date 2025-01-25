9af763c551cab62bc9ecc06fb89660f6c93a1aae
Anish Raj <evan.ansh@icloud.com>
2025-01-25 06:18:52 +0530

    build for production

--- public/js/app.js
+++ public/js/app.js
@@ -50574,7 +50574,7 @@ function h(tag, key, args) {
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "http://localhost/copper-crumb-clone/public/";
+/******/ 		__webpack_require__.p = "https://copper-crumb.wloper.com/public/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */

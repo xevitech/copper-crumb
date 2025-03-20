26a61255a046a224bfef5c419b9bef8d934f6a4b
Anish Raj <evan.ansh@icloud.com>
2025-03-20 19:05:20 +0530

    Update InvoiceEdit.vue

--- resources/js/components/admin/invoices/InvoiceEdit.vue
+++ resources/js/components/admin/invoices/InvoiceEdit.vue
@@ -388,6 +388,8 @@
                                     />
                                 </div>
 
+                                
+
                                 <div v-if="formData.payment_type == 'bank'">
                                     <div class="from-group">
                                         <label for="">{{ __("custom.account_number") }}</label>
